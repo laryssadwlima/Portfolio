7f31760f65eadd2bb9baefbf8c23b55041112bb4
Laryssa De Lima <130712370+laryssadwlima@users.noreply.github.com>
2025-01-14 12:15:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("nav a").forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
